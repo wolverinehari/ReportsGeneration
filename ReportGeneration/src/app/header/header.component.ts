ae5725473922fb81b2a16ba5ad2557d9f1ba275b
Subramanian <hsubrama@vv29458.attlocal.net>
2018-02-11 12:36:56 -0600

    Header username added

--- ReportGeneration/src/app/header/header.component.ts
+++ ReportGeneration/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ReportsdataserviceService} from '../reportsdataservice.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private reportsdataserviceService:ReportsdataserviceService) { }
+  selectedlogindata=this.reportsdataserviceService.selectedlogin[0];
 
   ngOnInit() {
   }
